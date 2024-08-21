91d776d4821726fb82d55ae334861fbaa8fae101
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-08-21 23:29:21 +0530

    datatypes

---
+++ datatypes/one.py
@@ -0,0 +1,12 @@
+price=399
+print(price)
+print(type(price))
+
+
+ename="rahul gandhi"
+avail='y'
+discount=False
+
+print(type(ename))
+print(type(avail))
+print(type(discount))
---
+++ datatypes/two.py
@@ -0,0 +1,33 @@
+eid =101
+esal=45000.00
+ename="rahul gandhi"
+avail=True
+c=100+300j
+r=range(10)
+l1=[10,20,30,40,50]
+t1=(10,20,30,40,50)
+s1={10,20,30,40,50}
+emp={'id':101,'name':"rohi"}
+b=bytes(l1)
+ba=bytearray(l1)
+fa=frozenset(s1)
+def wish():
+    pass
+
+print(type(eid))
+print(type(esal))
+print(type(ename))
+print(type(avail))
+print(type(c))
+print(type(r))
+print(type(l1))
+print(type(t1))
+print(type(s1))
+print(type(emp))
+print(type(b))
+print(type(ba))
+print(type(fa))
+print(type(wish()))
+print(b)
+print(ba)
+print(fa)
