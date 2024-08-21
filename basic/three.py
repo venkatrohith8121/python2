5a62fef0ea2ba59fe79d2a062a91bca1c469a54d
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2024-08-21 17:05:26 +0530

    identifers and functions

---
+++ basic/three.py
@@ -0,0 +1,8 @@
+# create function
+def wish():
+    print("good morning")
+# Read/invoking the function
+wish()
+# deleting the function
+del wish
+wish()
---
+++ basic/two.py
@@ -0,0 +1,10 @@
+# create variable
+price=399
+# read variable
+print(price)
+# update variable
+price=400
+print(price)
+# delete variable
+del price
+print(price)
